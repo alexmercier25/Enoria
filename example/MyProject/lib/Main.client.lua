de1b26f1803f864557d55eb888a82b123905044f
Buildex <alexandremercier25@gmail.com>
2021-04-05 15:09:45 -0400

    updated example

--- example/MyProject/lib/Main.client.lua
+++ example/MyProject/lib/Main.client.lua
@@ -1,8 +1,10 @@
-local Enoria = require(game.ReplicatedStorage.Enoria.packages.Enoria.Enoria)
+local Enoria = require(game.ReplicatedStorage.Common.Enoria.packages.Enoria.Enoria)
 local CustomTheme = require(script.Parent.Parent.style.Theme)
+local Classes = require(script.Parent.Parent.style.Classes)
 
 local e = Enoria.new()
 e.Context.Theme = CustomTheme
+e.Context.Classes = Classes
 
 e:RunApp({
 	Name = script.Parent.Parent.Name,
---
+++ example/MyProject/style/Classes.lua
@@ -0,0 +1,11 @@
+local Classes = {
+    Title = {
+        BackgroundColor3 = Color3.fromRGB(255, 0, 0),
+        TextTheme = {
+            TextSize = 32,
+            Font = Enum.Font.GothamSemibold
+        }
+    }
+}
+
+return Classes
